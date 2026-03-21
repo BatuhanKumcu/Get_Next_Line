e52d687b5ac74f0ab3da3a990b405a0a6d743a85
Batuhan Kumcu <agrega126@gmail.com>
2026-03-21 18:59:08 +0300

    main func prog

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,50 @@
 /*   By: bakumcu <bakumcu@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/18 14:10:30 by bakumcu           #+#    #+#             */
-/*   Updated: 2026/03/21 16:07:08 by bakumcu          ###   ########.fr       */
+/*   Updated: 2026/03/21 18:57:05 by bakumcu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+static char	*ft_read_store_line(int fd, char *stash)
+{
+	char	*buffer;
+	int		bytes;
+
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	while (!ft_strchr(stash, '\n') && bytes > 0)
+	{
+		bytes = read(fd, buffer, BUFFER_SIZE);
+		if (bytes < 0)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		buffer[bytes] = '\0';
+		stash = ft_strjoin(stash, buffer);
+	}
+	free(buffer);
+	return (stash);
+}
+
 char	*get_next_line(int fd)
 {
-	static char	*str;
-	char		*buffer;
-	int			bytes_read;
-
+	
 }
+
+/*
+* newline pos 
+* get the line
+* read the line 
+* free the line 
+* combine all in gnl
+*/
+
+/*
+* We basically have to grab a text inside a file and go until we see
+* newline. After the newline we take the line as the first line and 
+* remaining as leftover. we go until all lines end
+*/
