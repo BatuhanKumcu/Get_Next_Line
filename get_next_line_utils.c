1c0359ffd777ef0a6a7b0cce4592e45fcbec4b48
Batuhan Kumcu <agrega126@gmail.com>
2026-03-21 15:44:25 +0300

    utilsprog

---
+++ get_next_line_utils.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bakumcu <bakumcu@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/21 14:38:51 by bakumcu           #+#    #+#             */
+/*   Updated: 2026/03/21 15:41:34 by bakumcu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	
+}
